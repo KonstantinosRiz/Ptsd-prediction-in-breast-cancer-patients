9bb47e331aff580cffa460262de6b74ae3db964e
KonstantinosRiz <rizavask@gmail.com>
2023-01-09 19:46:04 +0200

    Cleanup model configs

--- source/models_config.R
+++ source/models_config.R
@@ -2,13 +2,3 @@ k_fold = 5
 
 # One of [none, down, up, smote, rose]
 sampling_method = "up"
-
-
-
-
-
-############## Possible things that can destroy me
-
-# Decision tree model: all surrogates are missing values as well
-
-# TODO: RE-CHECK THE ENTIRE FUCKING PREPROCESSING 
